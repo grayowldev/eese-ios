3457727672f90b5c7fa70f6c8816ac5ce7bd8f1d
kwasinsiah <truegrayowl@gmail.com>
2024-06-13 19:42:38 -0400

    Initial Commit

---
+++ Eese/EeseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EeseApp.swift
+//  Eese
+//
+//  Created by Kwasi Nsiah on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct EeseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
